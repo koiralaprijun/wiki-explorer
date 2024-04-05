09aca563909492cc13d9b12c1ac102187604a9ad
Prijun Koirala <prijun@kth.se>
2024-04-05 16:24:01 +0200

    Redirect Route

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/wiki/Earth",
+        permanent: true
+      }
+    ]
+  }
+}
+
+export default nextConfig
