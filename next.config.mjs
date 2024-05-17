0741cfeca01b4f5552a02a8f77ddb7e461d14504
Prijun Koirala <prijun@kth.se>
2024-05-17 15:40:50 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
             {
                 source: "/",
-                destination: "/wiki/Earth",
+                destination: "/welcome",
                 permanent: true,
             },
         ];
